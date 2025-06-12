220c119797f726e860d7998ade974145213faa34
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2025-06-12 09:47:39 -0300

    :recycle: refactor: Refactor to use params from route

--- src/app/Product/index.tsx
+++ src/app/Product/index.tsx
@@ -2,13 +2,9 @@ import { ButtonIcon } from "@/components/ButtonIcon";
 import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
 import { StackRoutesProps } from "@/routes/StackRoutes";
-import { useRoute } from "@react-navigation/native";
 import { Text, View } from "react-native";
 
-type RouteParams = StackRoutesProps<"product">;
 export const Product = ({ navigation, route }: StackRoutesProps<"product">) => {
-  const { params } = useRoute<RouteParams["route"]>();
-
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
@@ -19,8 +15,7 @@ export const Product = ({ navigation, route }: StackRoutesProps<"product">) => {
         <Title>Product</Title>
       </Header>
 
-      <Text>{params.id}</Text>
-      {/* <Text>{route.params.id}</Text> */}
+      <Text>{route.params.id}</Text>
     </View>
   );
 };
