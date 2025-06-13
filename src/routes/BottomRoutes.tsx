1e7a1a826eb870c23416ef8af815f88c5f1347fa
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2025-06-13 15:36:35 -0300

    :sparkles: feat: Config properties to bottom tab

--- src/routes/BottomRoutes.tsx
+++ src/routes/BottomRoutes.tsx
@@ -4,6 +4,7 @@ import {
   BottomTabScreenProps,
   createBottomTabNavigator,
 } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export type BottomRoutesList = {
   home: undefined;
@@ -19,10 +20,34 @@ export const BottomRoutes = () => {
   return (
     <Tab.Navigator
       initialRouteName={"home"}
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#2c4681",
+        tabBarInactiveTintColor: "#4444",
+        tabBarLabelPosition: "beside-icon",
+        animation: "shift",
+      }}
     >
-      <Tab.Screen name={"home"} component={Home} />
-      <Tab.Screen name={"product"} component={Product} />
+      <Tab.Screen
+        name={"home"}
+        component={Home}
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={"product"}
+        component={Product}
+        options={{
+          title: "Produto",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="add-circle" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
